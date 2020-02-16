408ba35f83d47fe055bf4e3341807f796b05e214
Spanaic <spanaic@gmail.com>
2020-02-16 18:11:18 +0900

    [add]HeaderとFooterの追加

---
+++ web/laravel/resources/js/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="navbar">
+    <RouterLink class="navbar__brand" to="/">Vuesplash</RouterLink>
+    <div class="navbar__menu">
+      <div class="navbar__item">
+        <button class="button">
+          <i class="icon ion-md-add">Submit a photo</i>
+        </button>
+      </div>
+      <span class="navbar__item">username</span>
+      <div class="navbar__item">
+        <RouterLink class="button button--link" to="/login">login / Register</RouterLink>
+      </div>
+    </div>
+  </nav>
+</template>
