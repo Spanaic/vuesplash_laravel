1e9de7c4ed73d7db48a07435a1b2cd26e0d7aa4a
Spanaic <spanaic@gmail.com>
2020-02-17 20:45:57 +0900

    [add]vuexの雛形追加

--- web/laravel/resources/js/app.js
+++ web/laravel/resources/js/app.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 // ルーティングの定義をインポートする
 import router from './router'
 // ルートコンポーネントをインポートする
+import store from './store'
+// vuexのstoreをインポートする
 import App from './App.vue'
 
 new Vue({
     el: '#app',
     router, //ルーティングの定義を読み込む
+    store, //storeを追加
     components: { App }, //ルートコンポーネントの使用を宣言する
     template: '<App />' //ルートコンポーネントを描画する
 })
