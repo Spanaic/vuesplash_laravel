a43cc77b028e6ad722eabf78d91d324b1f3dd0b6
Spanaic <spanaic@gmail.com>
2020-02-21 23:25:17 +0900

    [add]ルーティングとモデルへの記述を追加

--- web/laravel/app/User.php
+++ web/laravel/app/User.php
@@ -36,4 +36,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * リレーションシップ - photosテーブル
+     * @return \Illuminate\Database\Eloquent\Relations\HasOneOrMany
+     */
+    public function photos()
+    {
+        return $this->hasMany('App\Photo');
+    }
 }
