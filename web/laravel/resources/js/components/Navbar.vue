1786980faefff7215bfc196f28d330f289fb574b
Spanaic <spanaic@gmail.com>
2020-02-26 16:53:21 +0900

    [add]file uploadのコンポーネントの実装

--- web/laravel/resources/js/components/Navbar.vue
+++ web/laravel/resources/js/components/Navbar.vue
@@ -3,7 +3,7 @@
     <RouterLink class="navbar__brand" to="/">Vuesplash</RouterLink>
     <div class="navbar__menu">
       <div v-if="isLogin" class="navbar__item">
-        <button class="button">
+        <button @click="showForm = ! showForm" class="button">
           <i class="icon ion-md-add">Submit a photo</i>
         </button>
       </div>
@@ -12,11 +12,21 @@
         <RouterLink class="button button--link" to="/login">login / Register</RouterLink>
       </div>
     </div>
+    <PhotoForm v-model="showForm" />
   </nav>
 </template>
 
 <script>
+import PhotoForm from "./PhotoForm.vue";
 export default {
+  components: {
+    PhotoForm
+  },
+  data() {
+    return {
+      showForm: false
+    };
+  },
   computed: {
     // 算出プロパティで値の変化に応じてコンポーネントの表示を切り換える
     // 変化を見るのにv-ifを活用する
---
+++ web/laravel/resources/js/components/PhotoForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-show="value" class="photo-form">
+    <h2 class="title">Submit a photo</h2>
+    <form class="form">
+      <input type="file" class="form__item" />
+      <div class="form__button">
+        <button class="button button--inverse" type="submit">submit</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Boolean,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+</style>
